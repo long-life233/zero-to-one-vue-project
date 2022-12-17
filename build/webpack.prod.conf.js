217ff941007d452c46001eeb3ff3eefd8fa4666a
lukecheng <lukecheng233@163.com>
2022-12-17 16:19:03 +0800

    feat: 最初文件

--- build/webpack.prod.conf.js
+++
@@ -1,7 +0,0 @@
-const {merge} = require('webpack-merge')
-const baseWebpackConfig = require('./webpack.base.conf')
-const utils = require('./utils')
-
-module.exports = merge(baseWebpackConfig, {
-  plugins: [].concat(utils.htmlPlugin())
-})
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,3 @@
-const path = require('path');
-const {merge} = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const prodWebpackConfig = require('./build/webpack.prod.conf')
+module.exports = {
 
-module.exports = merge(prodWebpackConfig, {
-  context: path.resolve(__dirname,'./'), // entry 会将此目录作为根目录
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource'
-      }
-    ]
-  },
-  mode: 'production'
-})
+}
