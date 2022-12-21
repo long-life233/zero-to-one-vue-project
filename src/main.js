11b4a510f29456d43e764877feb56c9cba79ea32
lukecheng <lukecheng233@163.com>
2022-12-21 12:56:33 +0800

    feat: eslint，bable 配置

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+import { sayHi } from './utils/say'
+import './assets/css/style.css'
+import './assets/css/iconfont.css'
+
+import loveImg from './assets/img/love.png'
+
+sayHi()
+
+function component () {
+  const box = document.createElement('div')
+
+  const elment = document.createElement('h1')
+  elment.innerHTML = 'h1'
+  elment.classList.add('hello')
+  box.appendChild(elment)
+
+  const div = document.createElement('div')
+  div.classList.add('iconfont')
+  div.classList.add('icon-dianhuatianchong')
+  box.appendChild(div)
+
+  // 将图像添加到我们已经存在的 div 中。
+  const img = new Image()
+  img.src = loveImg
+  box.appendChild(img)
+
+  return box
+}
+
+document.body.appendChild(component())
---
+++ src/utils/say.js
@@ -0,0 +1,7 @@
+import _ from 'lodash'
+
+export const sayHi = () => {
+  // const msg = _.join(['hello', 'world'])
+  const msg = _.join(['hello', 'world', 'webpack 5！!!!!'])
+  console.log(msg)
+}
